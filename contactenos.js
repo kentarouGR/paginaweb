6068e48834dea3d1b9cfc5400a838756cdcd8fae
kentarouGR <76753305@continental.edu.pe>
2025-09-29 09:46:01 -0500

    versionultima

--- contactenos.js
+++ contactenos.js
@@ -1,23 +1,21 @@
-JS
 const inpNombre = document.getElementById('Coloca-nombre');
 const inpEmail = document.getElementById('Coloca-email');
 const inpMensaje = document.getElementById('Coloca-mensaje');
 
-async function enviardatos()
-{
-const nombre=inpNombre.value;
-const email=inpEmail.value;
-const mensaje=inpMensaje.value;
+function enviardatos() {
+  const nombre = inpNombre.value.trim();
+  const email = inpEmail.value.trim();
+  const mensaje = inpMensaje.value.trim();
 
-if (!nombre || !email || !mensaje )
-{
-    alert('Por favor rellene todos los campos');
+  if (!nombre || !email || !mensaje) {
+    alert('⚠️ Por favor, rellene todos los campos');
     return;
-}
-
-alert('Datos enviados correctamente');
-document.getElementById('Coloca-nombre').value = "";
-document.getElementById('Coloca-email').value = "";
-document.getElementById('Coloca-mensaje').value = "";
-  
   }
+
+  alert('Datos enviados correctamente');
+
+  // Limpiar campos
+  inpNombre.value = "";
+  inpEmail.value = "";
+  inpMensaje.value = "";
+}
